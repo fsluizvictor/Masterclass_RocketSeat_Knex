10a6945cab03d4c67586516b20023a8c4a6841b8
luiz victor <fs.luizvictor@gmail.com>
2020-07-11 18:31:12 -0300

    configuration of routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const express = require('express')
+const knex = require('./database')
+const routes = express.Router()
+
+
+routes.get('/users', (req, res) =>
+    knex('users').then((results) =>
+        res.json(results)))
+
+
+module.exports = routes  
