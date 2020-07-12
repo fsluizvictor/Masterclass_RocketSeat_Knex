f5495726924a3e7c62cee3fc83b129a84285065c
luiz victor <fs.luizvictor@gmail.com>
2020-07-11 21:32:09 -0300

    users's crud

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const express = require('express')
-const knex = require('./database')
+
 const routes = express.Router()
 
+const UserController = require('./controllers/UserController')
 
-routes.get('/users', (req, res) =>
-    knex('users').then((results) =>
-        res.json(results)))
+routes.get('/users',UserController.index)
+routes.post('/users',UserController.create)
+routes.put('/users/:id',UserController.update)
+routes.delete('/users/:id',UserController.delete)
 
 
 module.exports = routes  
